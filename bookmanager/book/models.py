ca619636a87fbff477ae3cebe9d6f4b124756801
ivea76-create <zhouh2209@gmail.com>
2024-11-11 19:44:38 +0800

    创建BookInfo类

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class BookInfo(models.Model):  # 定义一个book类，相当于定义一个数据库表
+    name = models.CharField(max_length=10)  # name= 相当于设置字段名 max_length与varchar()一样
+
+
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=10)
+    gender = models.BooleanField()  # 布尔类型(是男是女判断返回false或true)
+    # 外键约束
+    book = models.ForeignKey(BookInfo,)
