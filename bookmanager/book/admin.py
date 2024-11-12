983525eefbb314f7c706c6b76d76c38fc54ca321
ivea76-create <zhouh2209@gmail.com>
2024-11-12 18:01:54 +0800

    admin站点设置

--- bookmanager/book/admin.py
+++ bookmanager/book/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
 # Register your models here.
+# 注册模型
+from book.models import BookInfo, PeopleInfo
+admin.site.register(BookInfo)
+admin.site.register(PeopleInfo)
--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -3,10 +3,14 @@ from django.db import models
 
 class BookInfo(models.Model):  # 定义一个book类，相当于定义一个数据库表
     name = models.CharField(max_length=10)  # name= 相当于设置字段名 max_length与varchar()一样
+    def __str__(self):
+        return self.name  # 因为S
 
 
 class PeopleInfo(models.Model):
     name = models.CharField(max_length=10)
     gender = models.BooleanField()  # 布尔类型(是男是女判断返回false或true)
     # 外键约束
-    book = models.ForeignKey(BookInfo,)
+    book = models.ForeignKey(BookInfo,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
