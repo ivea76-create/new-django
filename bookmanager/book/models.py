92f753c7e961947888498a5446174271a890fa14
ivea76-create <zhouh2209@gmail.com>
2024-11-12 20:51:45 +0800

    app.py类的别名修改

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -3,14 +3,16 @@ from django.db import models
 
 class BookInfo(models.Model):  # 定义一个book类，相当于定义一个数据库表
     name = models.CharField(max_length=10)  # name= 相当于设置字段名 max_length与varchar()一样
+
     def __str__(self):
-        return self.name  # 因为S
+        return self.name  
 
 
 class PeopleInfo(models.Model):
     name = models.CharField(max_length=10)
     gender = models.BooleanField()  # 布尔类型(是男是女判断返回false或true)
     # 外键约束
-    book = models.ForeignKey(BookInfo,on_delete=models.CASCADE)
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
